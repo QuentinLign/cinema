2fae629eef63ce16f3d68444f2e407f478b017d8
Amine372 <nakhila@orange.fr>
2020-03-02 12:19:32 +0100

    ajouts d'accolades

--- Film-Review-Movie-Database/inscription/model_inscription.php
+++ Film-Review-Movie-Database/inscription/model_inscription.php
@@ -23,66 +23,79 @@ class model_connexion
     {
         return $nom;
     }
+  }
     public function getemail($email)
     {
         return $email;
     }
+  }
     public function getmdp($mdp)
     {
         return $mdp;
     }
+  }
     public function getlogin($login)
     {
         return $login;
     }
+  }
     public function getprenom($prenom)
     {
         return $prenom;
     }
+  }
     public function getid($id)
     {
         return $id;
     }
+  }
     public function setnom ($nom)
     {
     if(is_string($nom))
     {
       $this->_nom=$nom;
     }
+  }
     public function setemail ($email)
     {
     if(is_string($email))
     {
       $this->_email=$email;
     }
+  }
     public function setmdp ($mdp)
     {
     if(is_string($mdp))
     {
       $this->_mdp=$mdp;
     }
+  }
     public function setlogin ($login)
     {
     if(is_string($login))
     {
       $this->_login=$login;
     }
+  }
     public function setprenom ($prenom)
     {
     if(is_string($prenom))
     {
       $this->_prenom=$prenom;
     }
+  }
     public function setid ($id)
     {
     if(is_string($id))
     {
       $this->_id=$id;
     }
+  }
     public function settelephone ($telephone)
     {
     if(is_string($telephone))
     {
       $this->_telephone=$telephone;
     }
+  }
 ?>
