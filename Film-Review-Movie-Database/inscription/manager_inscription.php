1ed8c4e1a51077fb070e50ac5f2bf9a5a0bd2912
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-03-17 08:49:31 +0100

    Mise à jour de la BDD
    Mise à jour de la BDD

--- Film-Review-Movie-Database/inscription/manager_inscription.php
+++ Film-Review-Movie-Database/inscription/manager_inscription.php
@@ -5,8 +5,8 @@ class Manager{ //Déclaration de la classe Manager
 public function inscription($donnee){
 
       $bdd=new PDO('mysql:host=localhost;dbname=cinemapoo;charset=utf8', 'root', ''); //Connexion à la BDD
-    $req=$bdd->prepare('INSERT into inscription (login, nom, prenom, telephone, mdp,  email, role) VALUES(:login, :nom, :prenom, :telephone, :mdp, :email, :role)'); //Préparation de la table inscription avec les valeurs de la table
-    $req->execute(array('login'=>$donnee->getlogin(),'nom'=>$donnee->getnom(), 'prenom'=>$donnee->getprenom(),'telephone'=>$donnee->gettelephone(), 'mdp'=>md5($donnee->getmdp(), 'email'=>$donnee->getemail(),'role'=>$donnee->getrole()))); //Execution des requêtes
+    $req=$bdd->prepare('INSERT into inscription (nom, prenom, mail, login, mdp, role) VALUES(:nom, :prenom, :mail, :login, :mdp, :role)'); //Préparation de la table inscription avec les valeurs de la table
+    $req->execute(array('nom'=>$donnee->getnom(),'prenom'=>$donnee->getprenom(), 'mail'=>$donnee->getmail(),'login'=>$donnee->getlogin(), 'mdp'=>md5($donnee->getmdp(), 'role'=>$donnee->getrole()))); //Execution des requêtes
     $resultat = $req->fetch();
 	//Conditions de redirection
     if ($resultat ==true){
