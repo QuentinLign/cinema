bdda24997f839e3131411ec6e920a85aef3aec55
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-03-17 10:31:43 +0100

    Delete traitement_inscription.php

--- Film-Review-Movie-Database/inscription/traitement_inscription.php
+++
@@ -1,5 +0,0 @@
-<?php
-$inscription = new inscription($_POST["nom"], $_POST["prenom"], $_POST["mail"], $_POST["login"], $_POST["mdp"], $_POST["role"]);
-$signup = new Manager();
-$signup->inscription($inscription);
-?>
