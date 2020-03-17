6e1b882906673e56e12325977d74b34ca8cf9bd1
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-03-17 13:24:55 +0100

    Manager inscription à jour

---
+++ Film-Review-Movie-Database/inscription/manager_inscription.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'model_inscription.php';
+require_once 'traitement_inscription.php';
+class Manager{ //Déclaration de la classe Manager
+public function inscription($donnee){
+
+      $bdd=new PDO('mysql:host=localhost;dbname=cinemapoo;charset=utf8', 'root', ''); //Connexion à la BDD
+    $req=$bdd->prepare('INSERT into utilisateurs (nom, email, telephone, mdp) VALUES(:nom, :email, :telephone, :mdp)'); //Préparation de la table inscription avec les valeurs de la table
+    $req->execute(array('nom'=>$donnee->getnom(),'email'=>$donnee->getemail(), 'telephone'=>$donnee->gettelephone(), 'mdp'=>md5($donnee->getmdp()))); //Execution des requêtes
+    $a = $req->fetch();
+	//Conditions de redirection
+    if ($a ==true){
+     header("location: index.php");
+    }
+    else{
+     header("location: ../Connexion/connexion.php");
+    }
+
+          }
+}
+
+?>
