816e9c4d53a0612c612f00ee108de3e27f18f6ec
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-03-17 13:22:20 +0100

    Problème MD5 résolu - BDD à jour

--- Film-Review-Movie-Database/cinemapoo.sql
+++ Film-Review-Movie-Database/cinemapoo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 17 mars 2020 à 10:29
+-- Généré le :  mar. 17 mars 2020 à 12:21
 -- Version du serveur :  5.7.24
 -- Version de PHP :  7.2.14
 
@@ -50,9 +50,17 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `nom` varchar(30) COLLATE utf8_bin NOT NULL,
   `email` varchar(30) COLLATE utf8_bin NOT NULL,
   `telephone` varchar(15) COLLATE utf8_bin NOT NULL,
-  `mdp` varchar(20) COLLATE utf8_bin NOT NULL,
+  `mdp` varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `nom`, `email`, `telephone`, `mdp`) VALUES
+(1, 'test', 'qlignani@gmail.com', '097', '202cb962ac59075b964b07152d234b70'),
+(2, 'Miami', 'miami@gmail.com', '098765', '7d74f6896e07adce917c12a416944b0e');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
