253f0355922949816ac19861a2ef964c1ed2532a
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-03-17 08:50:57 +0100

    BDD à jour
    Modification du nom de la table

--- Film-Review-Movie-Database/inscription/manager_inscription.php
+++ Film-Review-Movie-Database/inscription/manager_inscription.php
@@ -5,7 +5,7 @@ class Manager{ //Déclaration de la classe Manager
 public function inscription($donnee){
 
       $bdd=new PDO('mysql:host=localhost;dbname=cinemapoo;charset=utf8', 'root', ''); //Connexion à la BDD
-    $req=$bdd->prepare('INSERT into inscription (nom, prenom, mail, login, mdp, role) VALUES(:nom, :prenom, :mail, :login, :mdp, :role)'); //Préparation de la table inscription avec les valeurs de la table
+    $req=$bdd->prepare('INSERT into utilisateurs (nom, prenom, mail, login, mdp, role) VALUES(:nom, :prenom, :mail, :login, :mdp, :role)'); //Préparation de la table inscription avec les valeurs de la table
     $req->execute(array('nom'=>$donnee->getnom(),'prenom'=>$donnee->getprenom(), 'mail'=>$donnee->getmail(),'login'=>$donnee->getlogin(), 'mdp'=>md5($donnee->getmdp(), 'role'=>$donnee->getrole()))); //Execution des requêtes
     $resultat = $req->fetch();
 	//Conditions de redirection
