a490d03cc3dcf306689e8809fdc973a9e912caf4
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-03-17 09:09:57 +0100

    TRAITEMENT à jour

--- Film-Review-Movie-Database/inscription/traitement_inscription.php
+++ Film-Review-Movie-Database/inscription/traitement_inscription.php
@@ -1,5 +1,5 @@
 <?php
-$inscription = new inscription($_POST["login"], $_POST["nom"], $_POST["prenom"], $_POST["telephone"], $_POST["mdp"], $_POST["email"], $_POST["role"]);
+$inscription = new inscription($_POST["nom"], $_POST["prenom"], $_POST["mail"], $_POST["login"], $_POST["mdp"], $_POST["role"]);
 $signup = new Manager();
 $signup->inscription($inscription);
 ?>
