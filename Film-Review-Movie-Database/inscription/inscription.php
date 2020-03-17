6fd759d67d339f75e77a18f238768bc938371bfa
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-03-17 09:18:34 +0100

    INSCRIPTION HTML à jour

--- Film-Review-Movie-Database/inscription/inscription.php
+++ Film-Review-Movie-Database/inscription/inscription.php
@@ -5,27 +5,33 @@
         <form method="post" action="#">
             <div class="row">
                  <label for="username-2">
-                    Nom d'uttilisateurs:
-                    <input type="text" name="username" id="username-2" placeholder="Hugh Jackman" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$" required="required" />
+                    Nom :
+                    <input type="text" name="nom" id="username-2" placeholder="Hugh Jackman" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$" required="required" />
                 </label>
             </div>
 
             <div class="row">
                 <label for="email-2">
-                    Votre mail:
-                    <input type="password" name="email" id="email-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
+                    Prenom :
+                    <input type="text" name="prenom" id="email-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
                 </label>
             </div>
              <div class="row">
                 <label for="password-2">
-                    Mot de passe:
-                    <input type="password" name="password" id="password-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
+                    Mail :
+                    <input type="mail" name="mail" id="password-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
                 </label>
             </div>
              <div class="row">
                 <label for="repassword-2">
-                    Retapez le mot de passe:
-                    <input type="password" name="password" id="repassword-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
+                   Login :
+                    <input type="text" name="login" id="repassword-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
+                </label>
+            </div>
+                         <div class="row">
+                <label for="password-2">
+                    Mot de passe :
+                    <input type="password" name="mdp" id="password-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
                 </label>
             </div>
            <div class="row">
