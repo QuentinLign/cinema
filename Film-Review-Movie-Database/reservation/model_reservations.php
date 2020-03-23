e927ca36983063d25e292710f1c801ea9e453a10
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-03-23 11:56:44 +0100

    Model réservation film

---
+++ Film-Review-Movie-Database/reservation/model_reservations.php
@@ -0,0 +1,77 @@
+<?php
+require_once 'manager_reservations.php';
+require_once 'traitement_reservations.php';
+class reservations { //Déclaration de la classe inscription
+//Déclaration des attributs
+  private $_login;
+  private $_mail;
+  private $_film;
+  private $_nb_places;
+  private $_paiement;
+
+
+  public function __construct($login, $mail, $film, $nb_places, $paiement){
+//Partie SET
+      $this->setlogin($login);
+      $this->setmail($mail);
+      $this->setfilm($film);
+      $this->setnb_places($nb_places);
+      $this->setpaiement($paiement);
+}
+
+public function setlogin($login){
+  if(empty($login)){
+    trigger_error('la variable doit etre un caractere');
+    return;
+  }
+  $this->_login = $login;
+}
+public function setmail($mail){
+  if(empty($mail)){
+    trigger_error('la variable doit etre un caractere');
+    return;
+  }
+  $this->_mail = $mail;
+}
+public function setfilm($film){
+  if(empty($film)){
+    trigger_error('la variable doit etre un caractere');
+    return;
+  }
+  $this->_film = $film;
+}
+public function setnb_places($nb_places){
+  if(empty($nb_places)){
+    trigger_error('la variable doit etre un caractere');
+    return;
+  }
+  $this->_nb_places = $nb_places;
+}
+public function setpaiement($paiement){
+  if(empty($paiement)){
+    trigger_error('la variable doit etre un caractere');
+    return;
+  }
+  $this->_paiement = $paiement;
+}
+
+//Partie Get
+public function getlogin(){
+  return $this->_login;
+}
+public function getmail(){
+  return $this->_mail;
+}
+public function getfilm(){
+  return $this->_film;
+}
+public function getnb_places(){
+  return $this->_nb_places;
+
+}
+public function getpaiement(){
+  return $this->_paiement;
+
+}
+}
+?>
