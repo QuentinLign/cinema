c3c0fc6d1c85d8bf053d4d515669ee599166b72d
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-03-30 10:05:03 +0200

    Manager réservation à jour

--- Film-Review-Movie-Database/reservation/manager_reservations.php
+++ Film-Review-Movie-Database/reservation/manager_reservations.php
@@ -5,15 +5,15 @@ class Manager{ //Déclaration de la classe Manager
 public function reservations($donnee){
 
       $bdd=new PDO('mysql:host=localhost;dbname=cinemapoo;charset=utf8', 'root', ''); //Connexion à la BDD
-    $req=$bdd->prepare('INSERT into reservations (login, mail, film, nbplaces, paiement) VALUES(:login, :mail, :film, :nbplaces, :paiement)'); //Préparation de la table inscription avec les valeurs de la table
-    $req->execute(array('login'=>$donnee->getlogin(),'mail'=>$donnee->getmail(), 'film'=>$donnee->getfilm(), 'nbplaces'=>$donnee->getnbplaces(),'paiement'=>$donnee->getpaiement())); //Execution des requêtes
+    $req=$bdd->prepare('INSERT into reservations (mail, film, nbplaces, paiement) VALUES(:mail, :film, :nbplaces, :paiement)'); //Préparation de la table inscription avec les valeurs de la table
+    $req->execute(array('mail'=>$donnee->getmail(), 'film'=>$donnee->getfilm(), 'nbplaces'=>$donnee->getnbplaces(),'paiement'=>$donnee->getpaiement())); //Execution des requêtes
     $a = $req->fetch();
 	//Conditions de redirection
     if ($a ==true){
-     header("location: index.html");
+     header("location: /cinema-master/Film-Review-Movie-Database/index.html");
     }
     else{
-    header("location: index.html");
+    header("location: /cinema-master/Film-Review-Movie-Database/index.html");
     }
 
           }
