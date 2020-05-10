26a2d89baacb6af3559f81e3bcdca30a366df2b1
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-05-10 17:23:32 +0200

    Confim à jour

--- Film-Review-Movie-Database/membre/confirm.php
+++ Film-Review-Movie-Database/membre/confirm.php
@@ -3,9 +3,9 @@ require 'inc/bootstrap.php';
 $db = App::getDatabase();
 
 if(App::getAuth()->confirm($db, $_GET['id'], $_GET['token'], Session::getInstance())){
-    Session::getInstance()->setFlash('danger', "Votre compte a bien été validé");
+    Session::getInstance()->setFlash('danger', "Votre compte a bien été validé"); //Message de validation du compte
     App::redirect('account.php');
 }else{
-    Session::getInstance()->setFlash('danger', "Ce token n'est plus valide");
+    Session::getInstance()->setFlash('danger', "Ce token n'est plus valide"); //Message d'erreur de la validation du compte
     App::redirect('login.php');
 }
