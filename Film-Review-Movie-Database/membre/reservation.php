74645cdc3fc2c03de22540a923e19cdc1137d967
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-05-10 17:25:28 +0200

    Réservations à jour

--- Film-Review-Movie-Database/membre/reservation.php
+++ Film-Review-Movie-Database/membre/reservation.php
@@ -1,34 +1,33 @@
 <?php
 require 'inc/bootstrap.php';?>
-<?php Session_Start(); ?>
 <?php require 'inc/header.php'; ?>
 <h1>Je réserve ! </h1>
 <br>
-<form action="" method="POST">
+<form action="/Film-Review-Movie-Database/membre/class/manager_reservation.php" method="POST">
 
     <div class="form-group">
         <label for="">Votre nom et prénom</label>
-        <input type="text" name="username" class="form-control"/>
+        <input type="text" name="nom" class="form-control"/ required>
     </div>
 
     <div class="form-group">
         <label for="">Email</label>
-        <input type="text" name="email" class="form-control"/>
+        <input type="email" name="mail" class="form-control"/ required>
     </div>
 
     <div class="form-group">
         <label for="">Nom du film</label>
-        <input type="password" name="password" class="form-control"/>
+        <input type="text" name="film" class="form-control"/ required>
     </div>
 
     <div class="form-group">
-        <label for="">Nombre de personnes <label>
-        <input type="password" name="password_confirm" class="form-control"/>
+        <label for="">Nombre de places</label>
+        <input type="text" name="nbplaces" class="form-control"/ required>
     </div>
 
+<br>
     <button type="submit" class="btn btn-primary">Je réserve</button>
 
 </form>
 
 <?php require 'inc/footer.php'; ?>
-  <script> window.alert("Page en construction"); </script>
