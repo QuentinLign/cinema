f40504af2e8af8f9a80b947462c97cee1c941eca
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-05-10 17:23:08 +0200

    account à jour

--- Film-Review-Movie-Database/membre/account.php
+++ Film-Review-Movie-Database/membre/account.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'class/traitement_account.php';
+require_once 'class/traitement_account.php'; //lien vers le traitement
 ?>
 
     <h1>Bonjour <?= $_SESSION['auth']->username; ?></h1>
@@ -17,15 +17,15 @@ require_once 'class/traitement_account.php';
 	<br>
 <center><h2>Bienvenue sur votre espace client !</h2></center>
 <center>
-<?php
+<?php //Affichage des informations de l'utilisateur
 try
 {
-  $bdd = new PDO('mysql:host=localhost;dbname=cinemapoo;charset=utf8', 'root', '');
+  $bdd = new PDO('mysql:host=localhost;dbname=cinemapoo;charset=utf8', 'root', ''); //Connexion à la BDD avec PDO
   
   
   $bdd -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-catch(Exception $e)
+catch(Exception $e) //Message d'erreur
 {
         die('Erreur : '.$e->getMessage());
 }
@@ -39,8 +39,8 @@ $reponse = $bdd->query('
 
 while ($donnees = $reponse->fetch())
 {
-  echo '<p><b>Votre login</b> ' . htmlspecialchars($donnees['username']) .
-  '<p> <b>Votre adresse email</b> : ' . htmlspecialchars($donnees['email']) .
+  echo '<p><b>Votre login</b> ' . htmlspecialchars($donnees['username']) . //Afficher le login de l'utilisateur
+  '<p> <b>Votre adresse email</b> : ' . htmlspecialchars($donnees['email']) . //Afficher l'adresse mail de l'utilisateur
       
   '</p>';
 
