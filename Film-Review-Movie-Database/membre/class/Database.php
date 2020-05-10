55b3de03341217e36f9a4cfac700e6b5ab1411a4
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-05-10 17:27:08 +0200

    Database à jour

--- Film-Review-Movie-Database/membre/class/Database.php
+++ Film-Review-Movie-Database/membre/class/Database.php
@@ -3,7 +3,7 @@ class Database{
 
     private $pdo;
 
-    public function __construct($login, $password, $database_name, $host = 'localhost'){
+    public function __construct($login, $password, $database_name, $host = 'localhost'){ //Function pour la connexion à la BDD avec function__construct
         $this->pdo = new PDO("mysql:dbname=$database_name;host=$host", $login, $password);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
